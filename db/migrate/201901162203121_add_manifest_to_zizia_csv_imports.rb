04feeac52fe295a589d760d5d4ce91344776c6d8
Jamie Little <jamie@jamielittle.org>
2019-07-30 08:52:31 -0500

    Rename database migrations

--- db/migrate/201901162203121_add_manifest_to_zizia_csv_imports.rb
+++ db/migrate/201901162203121_add_manifest_to_zizia_csv_imports.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
-class AddManifestToCsvImports < ActiveRecord::Migration[5.1]
+class AddManifestToZiziaCsvImports < ActiveRecord::Migration[5.1]
   def change
     add_column :zizia_csv_imports, :manifest, :string
   end
