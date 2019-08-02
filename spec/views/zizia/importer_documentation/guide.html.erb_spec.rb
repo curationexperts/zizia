ed802b9d065644cde050b49433234c5c9d8d86ab
Jamie Little <jamie@jamielittle.org>
2019-08-02 12:39:57 -0500

    Move importer_documentation views
    These are not currently being loaded because
    they aren't in the `zizia` folder.

---
+++ spec/views/zizia/importer_documentation/guide.html.erb_spec.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+require 'spec_helper'
+
+RSpec.describe "zizia/importer_documentation/guide.html.erb", type: :view do
+  before do
+    view.stub(:render_guide).and_return('<h1 id="title">Title</h1>')
+  end
+
+  it 'renders as html' do
+    render
+    expect(rendered).to match(/<h1 id="title">Title<\/h1>/)
+  end
+end
