905e049c3a9ff5e2b3f1c06902c31e89ff66eecf
Bess Sadler <bess@curationexperts.com>
2019-08-22 17:17:18 -0400

    Try again to fix

--- spec/zizia/csv_template_spec.rb
+++ spec/zizia/csv_template_spec.rb
@@ -5,6 +5,10 @@ require 'spec_helper'
 RSpec.describe Zizia::CsvTemplate do
   let(:csv_template) { described_class.new }
 
+  before do
+    Zizia.config.metadata_mapper_class = Zizia::HyraxBasicMetadataMapper
+  end
+
   it 'returns a CSV string based on the headers in the Hyrax mapper' do
     expect(csv_template.to_s).to eq(Zizia::HyraxBasicMetadataMapper.new.fields.join(','))
   end
