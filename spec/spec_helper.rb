f3a23e9ab9dda3594e9f0ae744fc3ceec68af6d4
Mark Bussey <mark@curationexperts.com>
2019-09-25 11:18:37 -0500

    Revert "Update coverage formatter for CI environments"

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -20,8 +20,6 @@ require 'zizia'
 require 'zizia/spec'
 require 'byebug'
 
-SimpleCov.formatter = Coveralls::SimpleCov::Formatter if ENV['CI']
-
 RSpec.configure do |config|
   config.filter_run focus: true
   config.run_all_when_everything_filtered = true
