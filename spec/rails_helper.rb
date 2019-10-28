a4706b24e1ef0a9d2b63eb360a287856323db90c
Jamie Little <jamie@jamielittle.org>
2019-10-28 11:55:47 -0500

    Use DCE's standard caypbara config
    This changes the dummy app to use the capybara
    config that is used in other recent projects.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -11,6 +11,7 @@ require 'ffaker'
 require 'hydra-role-management'
 require 'byebug'
 require 'rails-controller-testing'
+require 'selenium-webdriver'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
