b1440c3770632a1ced46d03c74ae626ee596bda2
Jamie Little <jamie@jamielittle.org>
2019-10-29 08:32:45 -0500

    Set import details pagination to 10 (#48)
    This sets the default pagination for `CsvImportDetail` objects
    to 10.
    
    See:
    
    https://github.com/kaminari/kaminari#configuring-default-per_page-value-for-each-model-by-paginates_per
    
    The page you are on is controlled with the page param. 1 will have 1-10, 2 will will have 10-20, etc.

--- spec/dummy/spec/system/csv_import_details_page_spec.rb
+++ spec/dummy/spec/system/csv_import_details_page_spec.rb
@@ -3,16 +3,18 @@ include Warden::Test::Helpers
 
 RSpec.describe 'viewing the csv import detail page' do
   let(:csv_import) { FactoryBot.create(:csv_import) }
-  let(:csv_import_detail) { FactoryBot.create(:csv_import_detail) }
+  let(:csv_import_detail) { FactoryBot.create_list(:csv_import_detail, 12) }
   let(:user) { FactoryBot.create(:admin) }
 
-  it 'displays the metadata when you visit the page' do
+  before do
     user.save
     csv_import.user_id = user.id
     csv_import.save
-    csv_import_detail.save
+    csv_import_detail.each(&:save)
     login_as user
+  end
 
+  it 'displays the metadata when you visit the page' do
     visit ('/csv_import_details/index')
     expect(page).to have_content('CSV Imports ID')
     click_on '1'
@@ -20,16 +22,20 @@ RSpec.describe 'viewing the csv import detail page' do
   end
 
   it 'has the dashboard layout' do
-    user.save
-    csv_import.user_id = user.id
-    csv_import.save
-    csv_import_detail.save
-    login_as user
-
     visit ('/csv_import_details/index')
     expect(page).to have_content('Your activity')
     visit ('csv_import_details/show/1')
     expect(page).to have_content('About the Import')
     expect(page).to have_content('Your activity')
+
+    visit('/csv_import_details/index')
+    expect(page).to have_content('Next')
+  end
+
+  it 'has pagination at 10' do
+    visit('/csv_import_details/index')
+    expect(page).to have_content('Next')
+    click_on 'Next'
+    expect(page).to have_content('Previous')
   end
 end
