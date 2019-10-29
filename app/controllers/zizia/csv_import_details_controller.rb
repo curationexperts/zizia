a1852e2e949a77e3b69794f4f7b621fd7963e67d
Jamie Little <jamie@jamielittle.org>
2019-10-29 13:24:31 -0500

    Initial sortable tables
    This adds server-side sorting for the
    columns on the `zizia_csv_import_details` table.
    
    Sorting the additional headers will require more
    complex queries or a different solution.
    
    Connected to curationexperts/in-house#420

--- app/controllers/zizia/csv_import_details_controller.rb
+++ app/controllers/zizia/csv_import_details_controller.rb
@@ -1,21 +1,30 @@
 # frozen_string_literal: true
 module Zizia
   class CsvImportDetailsController < ApplicationController
+    helper_method :sort_column, :sort_direction
     load_and_authorize_resource
     with_themed_layout 'dashboard'
 
     def index
-      @csv_import_details = Zizia::CsvImportDetail.order(:id).page csv_import_detail_params[:page]
+      @csv_import_details = Zizia::CsvImportDetail.order(sort_column + ' ' + sort_direction).page csv_import_detail_params[:page]
     end
 
     def show
-      @csv_import_detail = Zizia::CsvImportDetail.find(csv_import_detail_params["id"])
+      @csv_import_detail = Zizia::CsvImportDetail.find(csv_import_detail_params['id'])
     end
 
     private
 
+      def sort_column
+        Zizia::CsvImportDetail.column_names.include?(params[:sort]) ? params[:sort] : 'created_at'
+      end
+
+      def sort_direction
+        %w[asc desc].include?(params[:direction]) ? params[:direction] : 'desc'
+      end
+
       def csv_import_detail_params
-        params.permit(:id, :page)
+        params.permit(:id, :page, :sort, :direction)
       end
   end
 end
