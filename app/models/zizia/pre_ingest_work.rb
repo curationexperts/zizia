0a9b34d15c75b7168559bd7e86815663db054ea3
Jamie Little <jamie@jamielittle.org>
2019-11-04 13:02:01 -0600

    Add works to batch UI
    This changes the files view to include `PreIngestWorks`.
    
    The files are grouped by their `PreIngestWork`, which includes
    the `deduplication_key` and `date`. This also includes the `title`,
    which is looked up in solr via the `deduplication_key`. If this
    isn't present for a work it means that the work has not been indexed
    yet so a message saying that is displayed instead of the title.
    
    Additional attributes from solr could be added in the future.
    
    Connected to https://github.com/curationexperts/in-house/issues/426

--- app/models/zizia/pre_ingest_work.rb
+++ app/models/zizia/pre_ingest_work.rb
@@ -2,5 +2,15 @@
 module Zizia
   class PreIngestWork < ::ApplicationRecord
     has_many :pre_ingest_files
+
+    # Returns the title based on the deduplication_key if the work has been indexed to solr
+    # @return [String] the work's title
+    def title
+      return 'This work does not have a deduplication key.' if deduplication_key.nil?
+      solr_title = ActiveFedora::SolrService.get("deduplication_key_tesim:#{deduplication_key}")
+                                            .dig('response', 'docs', 0, 'title_tesim', 0)
+      return solr_title unless solr_title.nil?
+      'This work\'s metadata has not been indexed yet.'
+    end
   end
 end
--- spec/models/zizia/pre_ingest_work_spec.rb
+++ spec/models/zizia/pre_ingest_work_spec.rb
@@ -2,8 +2,23 @@
 require 'rails_helper'
 
 RSpec.describe Zizia::PreIngestWork do
-  let(:pre_ingest_work) { FactoryBot.create(:pre_ingest_work, deduplication_key: "42") }
+  let(:pre_ingest_work) { FactoryBot.create(:pre_ingest_work, deduplication_key: '42') }
+  let(:pre_ingest_work_indexed) { FactoryBot.create(:pre_ingest_work, deduplication_key: '43') }
+  let(:work) { Work.new(title: ['A Nice Title'], deduplication_key: '43') }
+
+  before do
+    work.save
+  end
+
   it 'has a deduplication_key' do
-    expect(pre_ingest_work.deduplication_key).to eq "42"
+    expect(pre_ingest_work.deduplication_key).to eq '42'
+  end
+
+  it 'can return that metadata has not been indexed yet' do
+    expect(pre_ingest_work.title).to eq('This work\'s metadata has not been indexed yet.')
+  end
+
+  it 'can return a title for a work based on the deduplication key' do
+    expect(pre_ingest_work_indexed.title).to eq('A Nice Title')
   end
 end
