914e7e2eff5cfdb38ecea0388d1f7f2ccf4ed287
Jamie Little <jamie@jamielittle.org>
2019-11-05 15:13:30 -0600

    Remove uniqueness constraint from deduplication_key
    Adding a unique constraint to the `deduplication_key` solved
    grouping for an initial import, but it causes problems when
    you re-import. After a re-import `PreIngestFile`s are added to
    the existing `PreIngestWork`s which are assoicated with existing
    `CsvImportDetail`s.

---
+++ db/migrate/20191105211127_remove_uniqueness_constraint_to_pre_ingest_work_deduplication_key.rb
@@ -0,0 +1,7 @@
+class RemoveUniquenessConstraintToPreIngestWorkDeduplicationKey < ActiveRecord::Migration[5.1]
+  def change
+    remove_index :zizia_pre_ingest_works, :deduplication_key if index_exists?(:zizia_pre_ingest_works, :deduplication_key)
+
+    add_index :zizia_pre_ingest_works, :deduplication_key, unique: false
+  end
+end
