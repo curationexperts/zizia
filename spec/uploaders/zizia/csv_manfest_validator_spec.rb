8d3dc5e59c3c2ba2e35ffa6b2956e51f8b0ee9bf
McClain Looney <mcclain@curationexperts.com>
2021-08-31 14:44:52 -0500

    Use HyraxBasicMetadataMapper to generate CsvValidator valid fields list

---
+++ spec/uploaders/zizia/csv_manfest_validator_spec.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+require 'spec_helper'
+
+require 'zizia/hyrax/hyrax_basic_metadata_mapper'
+RSpec.describe Zizia::CsvManifestValidator, type: :model do
+  before :all do
+    # patch in a fake initializer
+    class Zizia::CsvManifestValidator
+      def initialize
+      end
+    end
+  end
+  subject(:validator) { described_class.new }
+
+  it "thinks valid fields is the same as hyraxbasicmetadata.fields" do
+    expect(validator.send(:valid_headers).sort).to eq(Zizia::HyraxBasicMetadataMapper.new.headers.map(&:to_s).sort)
+  end
+end
