410f9faf802031d060c8970153042e16d83c76d2
Mark Bussey <mark@curationexperts.com>
2021-09-01 12:08:35 -0500

    Simplify the Batch Import form (#115)

---
+++ config/features.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+Flipflop.configure do
+  feature :new_zizia_ui,
+          default: false,
+          description: "Show new UI features and workflows in Zizia importer."
+end
--- lib/zizia/engine.rb
+++ lib/zizia/engine.rb
@@ -20,6 +20,10 @@ module Zizia
       app.config.assets.precompile << %w[zizia/application.js zizia/application.css]
     end
 
+    initializer "load_features" do
+      Flipflop::FeatureLoader.current.append(self)
+    end
+
     initializer :append_migrations do |app|
       unless app.root.to_s.match root.to_s
         config.paths['db/migrate'].expanded.each do |expanded_path|
