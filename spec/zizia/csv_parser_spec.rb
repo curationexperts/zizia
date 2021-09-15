c3f42d386b87bd7bef135bd0c796e9849bd1e93a
McClain Looney <mlooney@users.noreply.github.com>
2021-09-15 15:06:09 -0500

    Fixes some deprecation warnings, and allows import of CSV's with blank lines (#124)
    * Deprecation &  helper update
    
    * conditionally load coveralls
    * fix deprecation warnings for riiif & sqlite
    
    * Updates to allow the csv parser to ignore blank lines
    
    Co-authored-by: McClain Looney <mcclain@curationexperts.com>

--- spec/zizia/csv_parser_spec.rb
+++ spec/zizia/csv_parser_spec.rb
@@ -13,6 +13,7 @@ describe Zizia::CsvParser do
       <<-EOS
 title,description,date created
 The Moomins and the Great Flood,"The Moomins and the Great Flood (Swedish: Småtrollen och den stora översvämningen, literally The Little Trolls and the Great Flood) is a book written by Finnish author Tove Jansson in 1945, during the end of World War II. It was the first book to star the Moomins, but is often seen as a prelude to the main Moomin books, as most of the main characters are introduced in the next book.",1945
+
 Comet in Moominland,"Comet in Moominland is the second in Tove Jansson's series of Moomin books. Published in 1946, it marks the first appearance of several main characters, like Snufkin and the Snork Maiden.",1946
 EOS
     end
