21b6aaa9eb3a8f6b26491fe2c7e9a88540138a1c
oraposa <steve@curationexperts.com>
2021-09-15 12:07:00 -0500

    break comment into two lines

--- lib/zizia/engine.rb
+++ lib/zizia/engine.rb
@@ -25,7 +25,7 @@ module Zizia
     end
 
     initializer :append_migrations do |app|
-      unless app.root.to_s.match root.to_s
+      unless app.root.to_s.match? root.to_s
         config.paths['db/migrate'].expanded.each do |expanded_path|
           app.config.paths['db/migrate'] << expanded_path
         end
