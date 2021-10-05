fe7213799f841978d4d0441b6dab958f7b3665c6
Max Kadel <max@curationexperts.com>
2021-10-05 14:12:28 -0400

    Different type of timeout

--- spec/dummy/spec/support/capybara.rb
+++ spec/dummy/spec/support/capybara.rb
@@ -5,13 +5,15 @@ Capybara.default_max_wait_time = 20
 Capybara.server = :puma, { Silent: true }
 
 Capybara.register_driver :chrome_headless do |app|
+  client = Selenium::WebDriver::Remote::Http::Default.new
+  client.read_timeout = 120
+  client.open_timeout = 120
   options = ::Selenium::WebDriver::Chrome::Options.new
 
   options.add_argument('--headless')
   options.add_argument('--no-sandbox')
   options.add_argument('--disable-dev-shm-usage')
   options.add_argument('--window-size=1400,1400')
-
   Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
 end
 
